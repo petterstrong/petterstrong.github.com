ae7e517cdde4dbb8f334305b57710a0a40a9758a
james <742517689@qq.com>
2016-09-08 14:22:44 +0800

    new ui

--- framework/home.js
+++ framework/home.js
@@ -6,7 +6,7 @@
 		console.log("hey,man\n欢迎来到彼得·张的博客，很希望能一起交流，分享技术\n%c联系方式：jokemigo@163.com",'color: #f03');
 		$("#logo").on("click",function(){
 			$.ajax({
-				url:'https://api.thinkpage.cn/v3/weather/now.json?key=p7crmvka1l2alhbo&location=beijing&language=zh-Hans&unit=c',
+				url:'https://api.thinkpage.cn/v3/weather/now.json?location=beijing&ts=1443079775&ttl=30&uid=U5EEBB18CF&sig=uwBtAdd6J8Ll9um//O2NbwdRepE=&callback=?',
 				type:'get',
 				// callback:'showWeather',
 				dataType:'jsonp',
